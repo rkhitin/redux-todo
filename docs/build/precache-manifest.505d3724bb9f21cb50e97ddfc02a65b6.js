ecfcb400af4ff0d043f658be743a375e2cdeda65
Ron Khitin <ron@MacBook-Pro-Ron.local>
2019-01-29 23:43:03 +0300

    Change homepage

---
+++ docs/build/precache-manifest.505d3724bb9f21cb50e97ddfc02a65b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "293e135d6b1a2049b771",
+    "url": "/redux-todo/static/js/main.293e135d.chunk.js"
+  },
+  {
+    "revision": "47458cb786ceb4e20a3b",
+    "url": "/redux-todo/static/css/1.38139290.chunk.css"
+  },
+  {
+    "revision": "47458cb786ceb4e20a3b",
+    "url": "/redux-todo/static/js/1.47458cb7.chunk.js"
+  },
+  {
+    "revision": "e97dda427c85cde9b7b5",
+    "url": "/redux-todo/static/js/runtime~main.e97dda42.js"
+  },
+  {
+    "revision": "5dbae370414339a081c1c6fac80659cc",
+    "url": "/redux-todo/static/media/icons-16.5dbae370.eot"
+  },
+  {
+    "revision": "9d0578e5315c18aa528db1f1bfcb56f5",
+    "url": "/redux-todo/static/media/icons-20.9d0578e5.woff"
+  },
+  {
+    "revision": "26e98f292f486f2bfac2abf9472e490a",
+    "url": "/redux-todo/static/media/icons-16.26e98f29.ttf"
+  },
+  {
+    "revision": "d0bcd5b9effbe2570868f8c906ea7792",
+    "url": "/redux-todo/static/media/icons-16.d0bcd5b9.woff"
+  },
+  {
+    "revision": "0ec8ac2bacb2c3a1b2a2064305bc1223",
+    "url": "/redux-todo/static/media/icons-20.0ec8ac2b.ttf"
+  },
+  {
+    "revision": "f4d910a0626d8e60f310a7199dc6b191",
+    "url": "/redux-todo/static/media/icons-20.f4d910a0.eot"
+  },
+  {
+    "revision": "2f8712da99a4a75bd6d23928ff0292c8",
+    "url": "/redux-todo/index.html"
+  }
+];
